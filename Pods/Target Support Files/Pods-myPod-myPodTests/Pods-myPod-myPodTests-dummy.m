4198e59ee2b33ac67856028889b26089f92c9f8c
Abir Hcine <abir.hcine@mssolutions-group.com>
2020-04-13 09:17:24 +0100

    Initial commit

---
+++ Pods/Target Support Files/Pods-myPod-myPodTests/Pods-myPod-myPodTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_myPod_myPodTests : NSObject
+@end
+@implementation PodsDummy_Pods_myPod_myPodTests
+@end
---
+++ Pods/Target Support Files/Pods-myPod/Pods-myPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_myPod : NSObject
+@end
+@implementation PodsDummy_Pods_myPod
+@end
