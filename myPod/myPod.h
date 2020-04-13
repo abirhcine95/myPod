270f2256c7bf6c74301cd2737efca677cd29198b
Abir Hcine <abir.hcine@mssolutions-group.com>
2020-04-13 08:54:18 +0100

    Initial Commit

---
+++ myPod/myPod.h
@@ -0,0 +1,19 @@
+//
+//  myPod.h
+//  myPod
+//
+//  Created by mac on 13/04/2020.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for myPod.
+FOUNDATION_EXPORT double myPodVersionNumber;
+
+//! Project version string for myPod.
+FOUNDATION_EXPORT const unsigned char myPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myPod/PublicHeader.h>
+
+
